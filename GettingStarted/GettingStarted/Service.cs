4b04b426b93430c7ed4adb0d8f0050128149b5c6
Kamalesh-Periyasamy <Kamalesh.periyasamy@syncfusion.com>
2026-03-31 16:42:36 +0530

    added a pivot olap chart sample

---
+++ GettingStarted/GettingStarted/Service.cs
@@ -0,0 +1,114 @@
+﻿using Syncfusion.Olap.UWP.Common;
+using Syncfusion.Olap.UWP.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlapManagerService
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class Service1 : IOlapDataProvider
+    {
+        #region Private variables
+
+        private readonly OlapDataProvider _dataProvider;
+
+        #endregion
+
+        # region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Service1"/> class.
+        /// </summary>
+        public Service1()
+        {
+            string connectionString = "Data Source=https://bi.syncfusion.com/olap/msmdpump.dll; Initial Catalog=Adventure Works DW 2008 SE;";
+            _dataProvider = new OlapDataProvider(connectionString);
+        }
+
+        #endregion
+
+        # region IOlapDataProvider Members
+
+        public string GetJSONData(string mDXQuery, string currentReport, bool allowMdxToOlapReportParse)
+        {
+            CellSet cellSet = _dataProvider.ExecuteMdxQuery(mDXQuery, currentReport, allowMdxToOlapReportParse);
+            _dataProvider.DataProvider.CloseConnection();
+            return cellSet != null ? Utils.Serialize(cellSet) : string.Empty;
+        }
+
+        public string GetJSONCubeSchema(string cubeName)
+        {
+            CubeSchema cubeSchema = _dataProvider.GetCubeSchema(cubeName);
+            _dataProvider.DataProvider.CloseConnection();
+            return cubeSchema != null ? Utils.Serialize(cubeSchema) : string.Empty;
+        }
+
+        public string GetJSONCubes()
+        {
+            CubeInfoCollection cubeInfoCollection = _dataProvider.GetCubes();
+            _dataProvider.DataProvider.CloseConnection();
+            return cubeInfoCollection != null ? Utils.Serialize(cubeInfoCollection) : string.Empty;
+        }
+
+        public string GetJSONChildMembers(string cubeName, string levelName)
+        {
+            MemberCollection memberCollection = _dataProvider.GetChildMembers(cubeName, levelName);
+            _dataProvider.DataProvider.CloseConnection();
+            return memberCollection != null ? Utils.Serialize(memberCollection) : string.Empty;
+        }
+
+        public string GetJSONChildrenByMDX(string command)
+        {
+            MemberCollection memberCollection = _dataProvider.GetChildrenByMDX(command);
+            _dataProvider.DataProvider.CloseConnection();
+            return memberCollection != null ? Utils.Serialize(memberCollection) : string.Empty;
+        }
+
+        public string GetJSONDataWithTotalCount(string currentReport)
+        {
+            SerializableDictionary<string, object> count = _dataProvider.GetCellSetWithTotalCount(currentReport);
+            _dataProvider.DataProvider.CloseConnection();
+            return count != null ? Utils.Serialize(count) : string.Empty;
+        }
+
+        public string GetJSONExecuteDrillThroughQuery(string mdxQuery)
+        {
+            var executedQuery = _dataProvider.ExecuteDrillThroughQuery(mdxQuery);
+            _dataProvider.DataProvider.CloseConnection();
+            return executedQuery != null ? Utils.Serialize(executedQuery) : null;
+        }
+
+        public string GetJSONExecuteMemberCount(string mdxQuery)
+        {
+            var memberCount = _dataProvider.ExecuteMemberCount(mdxQuery);
+            _dataProvider.DataProvider.CloseConnection();
+            return memberCount >= 0 ? Utils.Serialize(memberCount) : string.Empty;
+        }
+
+        public string GetJSONLevelMembersUsingMdx(string memberUniqueName, int axisPosition, bool isGrandTotalOn, string cubeName, string pagingParams)
+        {
+            var levelMembers = _dataProvider.GetLevelMembersUsingMdx(memberUniqueName, axisPosition, isGrandTotalOn, cubeName, pagingParams);
+            _dataProvider.DataProvider.CloseConnection();
+            return levelMembers != null ? Utils.Serialize(levelMembers) : string.Empty;
+        }
+
+        public string GetJSONMeasureGroupsDimensions(string cubeName, string measureGroupName)
+        {
+            var dimensions = _dataProvider.GetMeasureGroupsDimensions(cubeName, measureGroupName);
+            _dataProvider.DataProvider.CloseConnection();
+            return dimensions != null ? Utils.Serialize(dimensions) : string.Empty;
+        }
+
+        public string GetJSONParentMember(string uniqueName, string currentCubeName)
+        {
+            var parentMember = _dataProvider.GetParentMember(uniqueName, currentCubeName);
+            _dataProvider.DataProvider.CloseConnection();
+            return parentMember != null ? Utils.Serialize(parentMember) : string.Empty;
+        }
+
+        #endregion
+    }
+}
